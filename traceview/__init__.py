e26be1cdee6b40896e7ee5c2a894fba05fc58480
Dan Riti <dmriti@gmail.com>
2014-04-17 13:54:35 +0000

    Add traceview directory.

---
+++ traceview/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+TraceView API library
+
+:copyright: (c) 2014 by Daniel Riti.
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'traceview'
+__version__ = '0.1.0'
+__author__ = 'Daniel Riti'
+__license__ = 'MIT'
