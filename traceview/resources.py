16e583ba14a57301e30a20b21e78424249aaddfc
Dan Riti <dmriti@gmail.com>
2014-04-18 22:38:05 +0000

    Update resources.py file.

--- traceview/resources.py
+++ traceview/resources.py
@@ -22,37 +22,53 @@ class Resource(object):
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def __call__(self):
-        """
-
-        """
-        return self.get()
+    def __call__(self, *args, **kwargs):
+        params = self.build_query_params(kwargs)
+        return self.get(params=params)
 
     @property
     def path(self):
         return "{0}/{1}/{2}".format(self.AUTHORITY, self.VERSION, self.PATH)
 
-    @property
-    def params(self):
-        return {"key": self.api_key}
+    def build_query_params(self, params=None):
+        """ Builds and returns a Dictionary of query parameters.
 
-    def get(self):
-        """
+        :params: (optional) Dictionary of query parameters.
 
         """
-        response = requests.get(self.path, params=self.params, allow_redirects=False)
-        if response.status_code == requests.codes.ok: # pylint: disable-msg=E1101
-            return response.json().get('data', None)
-        else:
-            return None
+        if not isinstance(params, dict):
+            params = {}
+        params.update({"key": self.api_key})
+        return params
+
+    def get(self, params=None):
+        """ Perform a HTTP GET request for the given Resource.
+
+        :params: (optional) Dictionary of query parameters.
+
+        """
+        if params is None:
+            params = {}
+
+        response = requests.get(self.path, params=params, allow_redirects=False)
+        if response.status_code != requests.codes.ok: # pylint: disable-msg=E1101
+            raise Exception(response.status_code, self.path, response.text)
+
+        return response.json()['data']
 
 
 class User(Resource):
+    """ Returns a List of user data for your organization.
+
+    """
 
     PATH = "organization/users"
 
 
 class Organization(Resource):
+    """ Returns a Dictionary of organization data.
+
+    """
 
     PATH = "organization"
 
@@ -62,5 +78,24 @@ class Organization(Resource):
 
 
 class App(Resource):
+    """ Returns a List of all available apps.
+
+    """
 
     PATH = "apps"
+
+
+class Layer(Resource):
+    """ Returns a List of all layers reporting data recently for the given app.
+    The default time window for reported layers is 1 day.
+
+    :param app: The app name to list layers.
+    :param since_time: (optional) The start of the time window as a UTC timestamp in milliseconds.
+
+    """
+
+    PATH = "layers/{app}"
+
+    def __call__(self, app, *args, **kwargs):
+        self.PATH = self.PATH.format(app=app)
+        return super(Layer, self).__call__(*args, **kwargs)
