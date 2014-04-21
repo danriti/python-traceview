3963a47608878d343b8056265bde966b244b9e9c
Dan Riti <dmriti@gmail.com>
2014-04-21 17:48:52 +0000

    Update readme and setup.py files.

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-traceview
+python-traceview
 =========
 
-TraceView API via Python
+TraceView API access via Python
+
+http://dev.appneta.com/docs/api-v2/reference.html
+
+## Installation
+
+To install python-traceview, simply:
+
+```bash
+$ pip install python-traceview
+```
+
+## Usage
+
+```python
+>>> import traceview
+>>> tv = traceview.TraceView("API KEY HERE")
+>>> tv.apps()
+[u'Default', u'pyramid_web_app']
+```
--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-
 import traceview
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 packages = []
 
-requires = []
+requires = ['requests==2.2.1']
+
+with open('README.md') as f:
+    readme = f.read()
 
 setup(
     name="python-traceview",
     version=traceview.__version__,
     description="TraceView API Client",
-    #long_description=long_description,
+    long_description=readme,
 
     # The project URL.
     url='https://github.com/danriti/python-traceview',
@@ -33,7 +39,6 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
@@ -41,12 +46,12 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
+        #'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.1',
+        #'Programming Language :: Python :: 3.2',
+        #'Programming Language :: Python :: 3.3',
     ],
 
     # What does your project relate to?
@@ -59,4 +64,6 @@ setup(
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed.
     install_requires=requires,
+
+    package_dir={'traceview': 'traceview'},
 )
