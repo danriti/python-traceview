d3349365f56de8caf709f2bca9e25deabde48901
Dan Riti <dmriti@gmail.com>
2014-04-21 20:11:57 +0000

    Update setup.py file.

--- setup.py
+++ setup.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python
 
-import traceview
+import codecs
+import os
+import re
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-packages = ['traceview']
+here = os.path.abspath(os.path.dirname(__file__))
 
-requires = ['requests==2.2.1']
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
 
-with open('README.md') as f:
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+with open('README.rst') as f:
     readme = f.read()
 
 setup(
     name="python-traceview",
-    version=traceview.__version__,
+    version=find_version('traceview', '__init__.py'),
     description="TraceView API Client",
     long_description=readme,
 
@@ -54,16 +71,17 @@ setup(
         #'Programming Language :: Python :: 3.3',
     ],
 
-    # What does your project relate to?
     keywords='traceview api client development performance',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages.
-    packages=packages,
+    packages=[
+        'traceview'
+    ],
 
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed.
-    install_requires=requires,
+    install_requires=[
+        'requests == 2.2.1'
+    ],
 
+    package_data={'': ['README.rst']},
     package_dir={'traceview': 'traceview'},
+    include_package_data=True,
 )
