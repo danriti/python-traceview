fd62c6d3059d656991c38b53ba7d5e4c9ce86099
Dan Riti <dmriti@gmail.com>
2014-05-02 23:41:06 +0000

    Add sphinx comments to TraceView properties #3

--- traceview/__init__.py
+++ traceview/__init__.py
@@ -28,20 +28,60 @@ class TraceView(object):
 
     :param api_key: The TraceView API access key.
 
+    Usage::
+
+      >>> import traceview
+      >>> tv = traceview.TraceView("API KEY HERE")
+
     """
 
     def __init__(self, api_key):
         self.api_key = api_key
 
+        #: Get all :py:class:`Actions <traceview.discovery.Action>` that have
+        #: been traced.
         self.actions = Action(self.api_key)
+
+        #: Get all available :py:class:`Apps <traceview.discovery.App>`.
         self.apps = App(self.api_key)
+
+        #: Get all :py:class:`Browsers <traceview.discovery.Browser>` used by
+        #: end users.
         self.browsers = Browser(self.api_key)
+
+        #: Get all :py:class:`Controllers <traceview.discovery.Controller>` that
+        #: have been traced.
         self.controllers = Controller(self.api_key)
+
+        #: Get all :py:class:`Domains <traceview.discovery.Domain>` that have
+        #: been traced.
         self.domains = Domain(self.api_key)
+
+        #: See :py:class:`Errors <traceview.errors.Rate>`.
         self.errors = Error(self.api_key)
+
+        #: Get all :py:class:`Hosts <traceview.discovery.Host>` that have been
+        #: traced.
         self.hosts = Host(self.api_key)
+
+        #: Get server or client latency information. For more details, check out
+        #: :py:class:`Series <traceview.latency.Series>`,
+        #: :py:class:`Summary <traceview.latency.Summary>`, and
+        #: :py:class:`ByLayer <traceview.latency.ByLayer>`.
         self.latency = Latency(self.api_key)
+
+        #: Get all recent :py:class:`Layers <traceview.discovery.Layer>` for an
+        #: application.
         self.layers = Layer(self.api_key)
+
+        #: Get all available :py:class:`Host Metrics <traceview.discovery.Metric>`
+        #: that have been collected.
         self.metrics = Metric(self.api_key)
+
+        #: Get :py:class:`Organization <traceview.organization.Organization>`
+        #: and :py:class:`User <traceview.organization.User>` information.
         self.organization = Organization(self.api_key)
+
+        #: Get all geographical :py:class:`Regions <traceview.discovery.Region>`
+        #: of end users.
         self.regions = Region(self.api_key)
