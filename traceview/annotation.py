7ae03db63523a9ca18b0da6adbce0d3c21d47fa1
Dan Riti <dmriti@gmail.com>
2014-09-02 23:09:06 +0000

    Update annotation module #12

--- traceview/annotation.py
+++ traceview/annotation.py
@@ -12,10 +12,23 @@ http://dev.appneta.com/docs/api-v2/annotations.html
 from .resource import Resource
 
 
-class Annotate(Resource):
+class Annotation(Resource):
 
     PATH = "log_message"
 
 class Annotations(Resource):
 
-    PATH = "annotations"
+    PATH_ALL = "annotations"
+    PATH_BY_APP = "app/{app}/annotations"
+
+    def get(self, app=None, *args, **kwargs):
+        """ Get annotations.
+
+        :param app: (optional) The app name.
+
+        """
+        if app:
+            self.path = self.PATH_BY_APP.format(app=app)
+        else:
+            self.path = self.PATH_ALL
+        return super(Annotations, self).get(*args, **kwargs)
