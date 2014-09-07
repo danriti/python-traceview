dd5a13f3e6d214d69c612232b7fa8ea96466bb3b
Dan Riti <dmriti@gmail.com>
2014-09-07 13:48:34 +0000

    Restructure Annotation path constants #12

--- traceview/annotation.py
+++ traceview/annotation.py
@@ -14,9 +14,11 @@ from .resource import Resource
 
 class Annotation(Resource):
 
-    PATH_POST = "log_message"
-    PATH_GET_ALL = "annotations"
-    PATH_GET_BY_APP = "app/{app}/annotations"
+    PATH = {
+        "POST"      : "log_message",
+        "GET"       : "annotations",
+        "GET_BY_APP": "app/{app}/annotations"
+    }
 
     def get(self, app=None, *args, **kwargs):
         """ Get annotations.
@@ -25,14 +27,14 @@ class Annotation(Resource):
 
         """
         if app:
-            self.path = self.PATH_GET_BY_APP.format(app=app)
+            self.path = self.PATH["GET_BY_APP"].format(app=app)
         else:
-            self.path = self.PATH_GET_ALL
+            self.path = self.PATH["GET"]
         return super(Annotation, self).get(*args, **kwargs)
 
     def post(self, *args, **kwargs):
         """ Post annotations.
 
         """
-        self.path = self.PATH_POST
+        self.path = self.PATH["POST"]
         return super(Annotation, self).post(*args, **kwargs)
