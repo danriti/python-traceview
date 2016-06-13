5667ce2fe8b26295308d664e7b3c37634288fada
Dan Riti <dmriti@gmail.com>
2016-06-13 01:41:38 +0000

    Update copyrights.

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'traceview'
-copyright = u'2014, Dan Riti'
+copyright = u'2016, Dan Riti'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
